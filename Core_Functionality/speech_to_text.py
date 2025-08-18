2a1f33f6284274b6347a966b97172f9a37c7614d
Talha-Jabar <jabbart29@gmail.com>
2025-08-18 12:06:21 +0500

    Main Functionality

--- Core_Functionality/speech_to_text.py
+++ Core_Functionality/speech_to_text.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-def transcribe_audio(audio_file_path: str, api_key: str) -> str:
+def transcribe_audio(audio_file_path: str, api_key: str | None) -> str:
     client = OpenAI(api_key=api_key)
     with open(audio_file_path, "rb") as audio_file:
         transcript = client.audio.transcriptions.create(
